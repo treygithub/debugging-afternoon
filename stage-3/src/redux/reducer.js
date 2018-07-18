90a6a1aab1f0c4d2ad49f8024b09252a013b91a7
robert hunnicutt <robhunn74@gmail.com>
2018-07-18 17:36:13 -0500

    fixed

--- stage-3/src/redux/reducer.js
+++ stage-3/src/redux/reducer.js
@@ -13,23 +13,28 @@ let initialState = {
 }
 
 // Reducer
-export default function reducer(state=initialState, action) {
-    switch(action.type) {
+function reducer(state = initialState, action) {
+    switch (action.type) {
         case GET_ALL_PRODUCTS_PENDING:
-            return Object.assign({}, state, {loading: true});
+            return Object.assign({}, state, { loading: true });
 
         case GET_ALL_PRODUCTS_FULFILLED:
-            return Object.assign({}, state, {loading: false, products: action.payload})
+            return Object.assign({}, state, {
+                loading: false,
+                products: action.payload
+            });
 
         case ADD_TO_SHOPPING_CART:
-            return Object.assign({}, state, {shoppingCart: [...state.shoppingCart, action.payload]});
+            return Object.assign({}, state, {
+                shoppingCart: [...state.shoppingCart, action.payload]
+            });
 
         case REMOVE_FROM_SHOPPING_CART:
             let newArray = state.shoppingCart.slice();
-            newArray.splice(action.index, 1);
-            return Object.assign({}, {shoppingCart: newArray});
-            
-        defualt:
+            newArray.splice(action.payload, 1);
+            return Object.assign({}, state, { shoppingCart: newArray });
+
+        default:
             return state;
     }
 }
@@ -55,3 +60,5 @@ export function getAllProducts(products) {
         payload: productsController.getAllProducts()
     }
 }
+
+export default reducer;
